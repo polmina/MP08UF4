2b30eac291a1786da0bec4e247014c5805b1aac4
X541U <X541U@LAPTOP-8M3QRRGJ>
2018-10-01 16:06:03 +0200

    Classe amb constructor creada

---
+++ src/mp08uf4/MP08UF4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp08uf4;
+
+/**
+ *
+ * @author X541U
+ */
+public class MP08UF4 {
+    
+    public MP08UF4(){
+        
+    }
+    
+    
+}
